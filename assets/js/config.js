1dcce6e5d4426a328cacc9a71c8e03f6b3e078a0
JuiPharm <jui_3065@hotmail.com>
2026-05-04 11:11:27 +0700

    Update API_URL to new deployment ID

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,6 +1,6 @@
 // Runtime config for Google Apps Script backend
 window.PE_CONFIG = {
-  API_URL: 'https://script.google.com/macros/s/YOUR_DEPLOYMENT_ID/exec',
+  API_URL: 'https://script.google.com/macros/s/AKfycbyIy7tJrZEAeesfARaBVgPaPCt4WXqcLRCIPOJ2_zPWxWCxWZO0pjYrJeCF6m-DEdjF/exec',
   API_MODE: 'jsonp', // jsonp is recommended for GitHub Pages -> Apps Script GET requests
   LOCK_API_URL: false,
   VERSION: 'gas-optimized-2026-04-29'
