a1bb9d0478134ae8fef210d5d873a33ee086f2c7
JuiPharm <jui_3065@hotmail.com>
2026-05-29 10:28:55 +0700

    Update API_URL in config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,6 +1,6 @@
 // Runtime config for Google Apps Script backend
 window.PE_CONFIG = {
-  API_URL: 'https://script.google.com/macros/s/AKfycbzD8Xn7sGxryra34N1caa6e5TrKVJSfWHNkf-EReOR7xbIZjcyPheT6yVHkJ8sDk1Cm/exec',
+  API_URL: 'https://script.google.com/macros/s/AKfycbyIy7tJrZEAeesfARaBVgPaPCt4WXqcLRCIPOJ2_zPWxWCxWZO0pjYrJeCF6m-DEdjF/exec',
   API_MODE: 'jsonp', // jsonp is recommended for GitHub Pages -> Apps Script GET requests
   LOCK_API_URL: false,
   VERSION: 'druggroup-fix-2026-05-04'
